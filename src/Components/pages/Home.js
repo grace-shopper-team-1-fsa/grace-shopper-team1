6f8b0db1d6fab9436f106706ad320ca0a62f34d8
Mayra Sanchez <sanchezmayra901@gmail.com>
2023-06-06 13:29:04 -0700

    added login/register

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { products } from '../../store';
 
 const Home = () =>{
+    const {products} = useSelector(state => state);
     return(
         <div>
             
