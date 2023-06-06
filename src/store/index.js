aed1685057f46ed29ab573b57f6c9f97bbea79ff
Mayra Sanchez <sanchezmayra901@gmail.com>
2023-06-06 10:02:39 -0700

    added slices to store index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,16 +2,28 @@ import {configureStore} from "@reduxjs/toolkit";
 import logger from 'redux-logger';
 import auth from './auth';
 import cart from './cart';
+import orders from './orders';
+import productsSlice from './productsSlice';
+import reviewSlice from './reviewSlice';
+import user from './user';
 
 const store = configureStore({
   middleware: (defaultMiddleware)=> defaultMiddleware().concat(logger),
   reducer:{
     auth: auth,
-    cart: cart
+    cart: cart,
+    orders: orders,
+    reviews: reviewSlice,
+    products: productsSlice,
+    users: user,
   }
 });
 
 export default store;
 export * from './auth';
 export * from './cart';
+export * from './orders';
+export * from './productsSlice';
+export * from './reviewSlice';
+export * from './user';
 
