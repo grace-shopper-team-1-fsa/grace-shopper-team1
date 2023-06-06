151015a4e96a984faa4084913ee776d8bbd559f1
Allen <aharper1609@gmail.com>
2023-06-06 14:37:02 -0700

    rending all product data within admin dashboard

--- src/Components/pages/Admin.js
+++ src/Components/pages/Admin.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { fetchProducts } from '../../store';
+
 
 const Admin = () =>{
+
+    const products = useSelector(state => state.products);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchProducts())
+    }, [dispatch])
+
+    console.log(products)
+
     return(
         <div>
-            
+            <h2>Product list</h2>
+            <ul>
+                {products.map((product) => {
+                    return ( 
+                        <li key={product.id}>
+                            <h3>{product.name}</h3>
+                            <p>Description: {product.description}</p>
+                            <img src={product.image} alt={product.name} />
+                            <p>Price: {product.price}</p>
+                            <p>Rating: {product.rating}</p>
+                            <button>update</button>
+                            <button>remove</button>
+                       </li>               
+                    )
+                })}       
+            </ul>
         </div>
     )
 }
