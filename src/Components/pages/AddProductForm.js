c68fde1ee551e0713f03fc507544971c8a7b071b
Allen <aharper1609@gmail.com>
2023-06-07 09:01:07 -0700

    added AddProductForm.js and updated app.js and pages/index.js

---
+++ src/Components/pages/AddProductForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AddProductForm() {
+    return (
+        <div>
+            <h1>Add New Product here</h1>
+        </div>
+    );
+}
+
+export default AddProductForm;
--- src/Components/pages/UpdateUserForm.js
+++ src/Components/pages/UpdateUserForm.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 const UpdateUserForm = () =>{
+    const { id } = useParams();
+    const dispatch = useDispatch();
+
+    const [username, setUsername] = useState('');
+    const [userFirstName, setUserFirstName] = useState('')
+    const [userLastName, setUserLastName] = useState('')
+
+
     return (
         <div>
             <h3>Hello from Update User Form</h3>
