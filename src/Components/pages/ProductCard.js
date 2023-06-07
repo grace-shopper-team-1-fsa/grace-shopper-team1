ff0d3402235d84c35ab488492be19e6cd15f81c7
Mayra Sanchez <sanchezmayra901@gmail.com>
2023-06-07 11:46:18 -0700

    added Link to product card

--- src/Components/pages/ProductCard.js
+++ src/Components/pages/ProductCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ProductCard = (props) =>{
     const {product} = props;
     return(
-        <div>
+        <Link to={`/product/${product.id}`}>
             <img src={window.location.origin + `${product.image}`} width="200" height="150" alt="Image"/>
             <p>{product.name}</p>
             <p>{product.price}</p>
-        </div>
+        </Link>
     )
 }
 
