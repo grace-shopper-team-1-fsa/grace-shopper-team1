27e0eb39a1398d12d2730918f3dd2dc40002dc86
Mayra Sanchez <sanchezmayra901@gmail.com>
2023-06-08 15:53:09 -0700

    modified cors

--- server/api/index.js
+++ server/api/index.js
@@ -14,12 +14,6 @@ router.use('/products', productRouter);
 router.use('/reviews', reviewRouter);
 router.use('/users', userRouter);
 
-router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://vaseshopper.onrender.com"); // update to match the domain you will make the request from
-   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
 router.get('/', (req, res, next) => {
     res.send('at /api')
 })
--- server/app.js
+++ server/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const routes = require('./api');
+const cors = require('cors');
 
+app.use(cors());
 
 app.use(express.json());
 
@@ -11,6 +13,11 @@ app.use('/static', express.static(path.join(__dirname, '../static')));
 
 app.use('/api', routes);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://vaseshopper.onrender.com"); // update to match the domain you will make the request from
+   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../static/index.html')));
 
