3c86741fca798f661e8fdb6c2e52c11cbaae470c
Mayra Sanchez <sanchezmayra901@gmail.com>
2023-06-08 15:57:04 -0700

    adding cors options

--- server/app.js
+++ server/app.js
@@ -4,7 +4,13 @@ const path = require('path');
 const routes = require('./api');
 const cors = require('cors');
 
-app.use(cors());
+const corsOptions ={
+    origin:'*',
+    credentials:true,      //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
