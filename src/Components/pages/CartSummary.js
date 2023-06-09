a0ce5d582c9e7b00fda16bcdf50bcc4bca16bcfc
Allen <aharper1609@gmail.com>
2023-06-09 08:25:57 -0700

    cart summary page

--- src/Components/pages/CartSummary.js
+++ src/Components/pages/CartSummary.js
@@ -1,23 +1,35 @@
 import React from 'react';
 
+
+
 const CartSummary = (props) => {
     const itemsToCalc = props;
+    let itemTotal = 0
 
-    console.log("FROM CART SUMMARY", itemsToCalc)
+    Object.keys(itemsToCalc).forEach(item => {
+        const lineItems = itemsToCalc[item];
+        lineItems.forEach(obj => {
+            itemTotal += obj.quantity * obj.product.price;       
+        })
+    })
+
 
     return(
-    <div className='summary'>
+    <div>
         <p>Summary</p>
         <div>
-            <div className='summaryRow'>
-                <p>Items: </p>
-                <p>${total}</p>
-            </div>
-            <div className='summaryRow totalRow'>
-                <p>Total</p>
-                <p>${total}</p>
-            </div>
-        </div>    
+        {
+            itemsToCalc.items.map(item => {
+                return (
+                    <div key={item.id}>
+                        <p>Name: {item.product.name}</p>
+                        <p>Quantity: {item.quantity}</p>
+                        <p>Subtotal: {item.quantity * item.product.price}</p>
+                    </div>)
+            })
+        }
+        </div>
+            <p>Total: {itemTotal}</p>
     </div>
     )
 }
