ff64142708eaff0040889c87e26c1ac25ff00cba
Neurotrip <ironlightcreative@gmail.com>
2023-06-13 14:48:16 -0500

    Updated Cart

--- src/Components/pages/Cart.js
+++ src/Components/pages/Cart.js
@@ -1,55 +1,93 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCart, fetchProducts } from '../../store';
-import {useNavigate} from 'react-router-dom';
-import {LineItem} from './';
-import {CartSummary} from './';
+import { useNavigate } from 'react-router-dom';
+import { LineItem, CartSummary } from './';
 
-const Cart = () =>{
-    const {auth} = useSelector(state=>state);
-    let guest = true;
-    let cart = {};
-    if(auth.id){
-        cart = useSelector(state => state.cart);
-        guest = false;
-    } else {
-        cart = JSON.parse(window.localStorage.getItem('cart'));
-        cart.total = 0;
-        cart.lineItems.forEach(e=> {
-            console.log(e.product.price + ' ' + e.quantity);
-            console.log(cart.total);
-            cart.total += e.product.price*e.quantity
-        });
-        console.log(cart.total);
-        window.localStorage.setItem('cart', JSON.stringify(cart));
-    }
-   
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+const AuthenticatedCart = () => {
+  const cart = useSelector(state => state.cart);
+  const guest = false;
+  const navigate = useNavigate();
 
-    useEffect(()=>{
-        dispatch(fetchCart());
-        dispatch(fetchProducts());
-    }, [dispatch]);
+  const handleClick = () => {
+    navigate('/checkout');
+  };
 
-    const handleClick = () =>{
-        navigate('/checkout');
-    }
-
-    return (
-        <div>
-            <div className='basket'>
-                <h2 className='shopping-cart-header'>Shopping Cart</h2>
-                { cart.lineItems.length > 0 ?
-                    cart.lineItems.map(lineItem=>{
-                        return <LineItem key={lineItem.id} guest={guest} lineItem={lineItem} product={lineItem.product}/>
-                    })
-                : <p className='cart-is-empty'>Cart is Empty</p>}
-            </div>
-            <CartSummary total={cart.total}/>
-            <button className='to-checkout-button' onClick={handleClick}>Continue to Checkout</button>
-        </div>
+  return (
+    <div>
+      <div className='basket'>
+        <h2 className='shopping-cart-header'>Shopping Cart</h2>
+        {cart.lineItems.length > 0 ? (
+          cart.lineItems.map(lineItem => (
+            <LineItem key={lineItem.id} guest={guest} lineItem={lineItem} product={lineItem.product} />
+          ))
+        ) : (
+          <p className='cart-is-empty'>Cart is Empty</p>
+        )}
+      </div>
+      <CartSummary total={cart.total} />
+      <button className='to-checkout-button' onClick={handleClick}>
+        Continue to Checkout
+      </button>
+    </div>
   );
-        
-}
+};
+
+const GuestCart = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const cartFromLocalStorage = JSON.parse(window.localStorage.getItem('cart'));
+  let cart = { lineItems: [], total: 0 };
+  const guest = true;
+
+  useEffect(() => {
+    dispatch(fetchCart());
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  if (cartFromLocalStorage) {
+    cart = { ...cartFromLocalStorage, total: 0 };
+    cart.lineItems.forEach(e => {
+      console.log(e.product.price + ' ' + e.quantity);
+      console.log(cart.total);
+      cart.total += e.product.price * e.quantity;
+    });
+    console.log(cart.total);
+    window.localStorage.setItem('cart', JSON.stringify(cart));
+  }
+
+  const handleClick = () => {
+    navigate('/checkout');
+  };
+
+  return (
+    <div>
+      <div className='basket'>
+        <h2 className='shopping-cart-header'>Shopping Cart</h2>
+        {cart.lineItems.length > 0 ? (
+          cart.lineItems.map(lineItem => (
+            <LineItem key={lineItem.id} guest={guest} lineItem={lineItem} product={lineItem.product} />
+          ))
+        ) : (
+          <p className='cart-is-empty'>Cart is Empty</p>
+        )}
+      </div>
+      <CartSummary total={cart.total} />
+      <button className='to-checkout-button' onClick={handleClick}>
+        Continue to Checkout
+      </button>
+    </div>
+  );
+};
+
+const Cart = () => {
+  const { auth } = useSelector(state => state);
+
+  if (auth.id) {
+    return <AuthenticatedCart />;
+  } else {
+    return <GuestCart />;
+  }
+};
+
 export default Cart;
