2bcd70d92fe56290ef60fa5b73576458b1975019
Mayra Sanchez <sanchezmayra901@gmail.com>
2023-06-13 12:45:24 -0700

    removed console.logs, testing done

--- src/store/auth.js
+++ src/store/auth.js
@@ -4,7 +4,6 @@ const initialState = {};
 
 export const loginWithToken = createAsyncThunk("loginWithToken", async (_, { rejectWithValue }) => {
   const token = window.localStorage.getItem('token');
-  console.log(token);
   if(token){
     const response = await axios.get('/api/auth', null,{
       headers: {
@@ -19,7 +18,6 @@ export const loginWithToken = createAsyncThunk("loginWithToken", async (_, { rej
     return userWithPermissions;
   }
   else {
-    console.log('in else')
     return rejectWithValue();
   }
 });
