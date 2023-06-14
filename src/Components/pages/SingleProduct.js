7a6a24b177b5be76e718c541ebc5d7265d4d57db
Neurotrip <ironlightcreative@gmail.com>
2023-06-14 14:30:12 -0500

    added back link on singleProduct page

--- src/Components/pages/SingleProduct.js
+++ src/Components/pages/SingleProduct.js
@@ -1,53 +1,60 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { fetchProductById } from '../../store/productsSlice';
 import { fetchAllReviews } from '../../store/reviewSlice';
 import { addItem } from '../../store/cart';
-import {ReviewForm} from './';
+import { ReviewForm } from './';
 import ReactModal from 'react-modal';
 
 const SingleProduct = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
   const product = useSelector((state) => state.products.find((product) => product.id === id));
-  const reviews = useSelector((state) => state.reviews.reviewsList.filter((review) => review.productId === id));
-  const {auth} = useSelector(state => state);
+  const reviews = useSelector((state) =>
+    state.reviews.reviewsList.filter((review) => review.productId === id)
+  );
+  const { auth } = useSelector((state) => state);
   const [quantity, setQuantity] = useState(1);
   const [open, setOpen] = useState(false);
-  
+
   useEffect(() => {
     dispatch(fetchProductById(id));
     dispatch(fetchAllReviews());
     setOpen(false);
   }, [dispatch, id]);
-  
+
   if (!product) {
     return <p>Loading...</p>;
   }
 
   const handleClick = () => {
-    if(auth.id){
-      dispatch(addItem({
-        product: product, 
-        quantity : quantity,
-      }))
+    if (auth.id) {
+      dispatch(
+        addItem({
+          product: product,
+          quantity: quantity,
+        })
+      );
       window.location.reload();
     } else {
       const cart = JSON.parse(window.localStorage.getItem('cart'));
-      const match = cart.lineItems.find(e=>e.product.id === product.id);
-      if(match){
+      const match = cart.lineItems.find((e) => e.product.id === product.id);
+      if (match) {
         match.quantity += quantity;
       } else {
-        cart.lineItems.push({product:product, quantity: quantity, productId: product.id});
+        cart.lineItems.push({ product: product, quantity: quantity, productId: product.id });
       }
       window.localStorage.setItem('cart', JSON.stringify(cart));
       window.location.reload();
     }
-  }
+  };
 
   return (
     <div className="product-page">
+      <Link to="/" className="back-link">
+        Back to Vases
+      </Link>
       <div className="product-page-box">
         <div className="product-image">
           <img src={product.image} alt={product.name} />
@@ -65,7 +72,7 @@ const SingleProduct = () => {
           <p>Price: ${product.price}</p>
         </div>
         <div className="product-quantity">
-        <input type="number" min="1" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
+          <input type="number" min="1" value={quantity} onChange={(e) => setQuantity(e.target.value)} />
         </div>
         <div className="product-rating">
           <p>Rating: {product.rating} / 5</p>
@@ -76,11 +83,13 @@ const SingleProduct = () => {
               <h3>{review.name}</h3>
               <p>Rating: {review.rating} / 5</p>
               <p>{review.description}</p>
-        </div>
+            </div>
           ))}
         </div>
-        <button className="add-to-cart" onClick={handleClick}>Add to Cart</button>
-        <button onClick={() => setOpen(true)}>Write a review!</button>    
+        <button className="add-to-cart" onClick={handleClick}>
+          Add to Cart
+        </button>
+        <button onClick={() => setOpen(true)}>Write a review!</button>
         <ReactModal isOpen={open}>
           <ReviewForm open={open} />
           <button onClick={() => setOpen(false)}>Cancel</button>
